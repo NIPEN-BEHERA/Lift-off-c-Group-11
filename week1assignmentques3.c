4fb423e07bf4b22e34c8ebec77e50cedbf80cae1
NIPEN-BEHERA <126486655+NIPEN-BEHERA@users.noreply.github.com>
2023-03-09 22:52:32 +0530

    Add files via upload

---
+++ week1assignmentques3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main() {
+ int num,sum;
+ printf("Enter integer numbers: ");
+ scanf ("%d", &num);
+ sum = 0;
+ do {
+  sum += num % 10; 
+  num /= 10; 
+ } while (num > 0);
+ printf ("The Sum is = %d \n",sum);
+}
